76726900828550d056c2e513b26b9a73f2fd7749
Cavendish McKay <cmckay@tachycline.com>
2018-05-21 07:38:54 -0400

    revisions to HW 10

--- homework/HW10.py
+++ homework/HW10.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 DATAFILE = "../data/sdss-trimmed.pkl"
 
@@ -53,8 +54,47 @@ def make_tranche(df, selection_value, selector_col="DIST_ADOP", selection_width=
 
 # The following functions are for you to fill in:
 
-def single_hist():
-    pass
+def single_hist(df, percentile, ax=None, large=True):
+    """Plot a single histogram """
+    
+    minval = df['DIST_ADOP'].quantile(percentile-0.005)
+    maxval = df['DIST_ADOP'].quantile(percentile+0.005)
+    selector = (df['DIST_ADOP'] > minval) & (df['DIST_ADOP'] < maxval)
+    subset = df[selector]
 
-def hist_array():
-    pass
+    if ax is None:
+        fig, ax = plt.subplots(1)
+    else:
+        fig = ax.get_figure()
+        
+
+    if large:
+        fig.set_figwidth(12)
+        fig.set_figheight(9)
+        vals, bins, patches = ax.hist(subset['TEFF_UNC_REL'], bins=50, rwidth=0.9)
+
+        maxbin = bins[np.argmax(vals)+1]
+        maxbinval = vals[np.argmax(vals)]    
+    
+        ax.set_xlabel("Relative uncertainty")
+        ax.set_ylabel("Number of observations")
+        ax.set_title(r"Histogram of Relative Uncertainties in $T_{eff}$ for " +
+                     "{} stars in the {} percentile of distance".format(len(subset), percentile*100),fontsize=18 )
+
+        ax.text(maxbin, maxbinval,"Max: {} at {:5.3e}".format(maxbinval, maxbin),
+                verticalalignment="center", horizontalalignment="left", fontsize=14)
+    else: # small
+        vals, bins, patches = ax.hist(subset['TEFF_UNC_REL'], bins=50, histtype="step")
+        ax.set_axis_off()
+                 
+                 
+def hist_array(df):
+    """Make an array of histograms."""
+    
+    fig, axes = plt.subplots(10,10)
+    fig.set_figwidth(12)
+    fig.set_figheight(12)
+    nplots = axes.size
+    for plot in zip(axes.flat, np.linspace(0.1, 0.99, nplots)):
+        ax = plot[0]
+        single_hist(df, plot[1], ax, large=False)
